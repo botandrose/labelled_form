6b1cd1d5d5cf1c0477c9cc339c3d5fa6ce6193b4
Micah Geisel <micah@botandrose.com>
2022-12-16 17:30:24 -0600

    add lots of new options.

--- README.md
+++ README.md
@@ -7,28 +7,42 @@ Adds `label:` option to Rails form helpers.
 Works like you'd expect, or at least, works like I'd expect! Examples:
 
 ```ruby
-form_with :user do |form|
+= form_with :user do |form|
 
   # Prepends label to field and infers label text
-  form.text_field :email, label: true
+  = form.text_field :email, label: true
   # <label for="user_email">Email</label>
   # <input type="text" name="user[email]" id="user_email">
 
   # Supports specifying different label text
-  form.text_field :name, label: "Full name"
+  = form.text_field :name, label: "Full name"
   # <label for="user_name">Full name</label>
   # <input type="text" name="user[name]" id="user_name">
 
   # Appends label to field for radio buttons and checkboxes
-  form.radio_button :gender, "Male", label: true
+  = form.radio_button :gender, "Male", label: true
   # <input type="radio" name="user[gender]" value="Male" id="user_gender_male">
   # <label for="user_gender_male">Male</label>
 
+  # Specify label class
+  = form.text_field :email, label_class: "hidden"
+  # <label for="user_email" class="hidden">Email</label>
+  # <input type="text" name="user[email]" id="user_email">
+
+  # Append colon to label
+  = form.text_field :email, label_with_colon: true
+  # <label for="user_email">Email:</label>
+  # <input type="text" name="user[email]" id="user_email">
+
+  # Wrap whole thing in a <dfn>
+  = form.text_field :email, wrap_in_dfn: true
+  # <dfn>
+  #   <dt><label for="user_email">Email</label></dt>
+  #   <dd><input type="text" name="user[email]" id="user_email"></dd>
+  # </dfn>
+
 end
 ```
-
-## Todo
-Figure out how to support options for the label tag, like `class:` and, uh, idk, other stuff. Maybe just pass options hash directly to `label:`? Maybe just start with additional `label_class:` option? Is there any other potential option for e.g. `form.label` we would care about? Probably should just cross this bridge when we get to it.
 
 ## Contributing
 
@@ -37,3 +51,4 @@ Bug reports and pull requests are welcome on GitHub at https://github.com/botand
 ## License
 
 The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+
--- lib/labelled_form.rb
+++ lib/labelled_form.rb
@@ -9,52 +9,84 @@ module LabelledForm
   end if defined?(Rails)
 
   class Builder < ActionView::Helpers::FormBuilder
-    def text_area method, options = {}
-      options = options.dup
-      if label_text = options.delete(:label)
-        label_text = method.to_s.humanize if label_text === true
-        label(method, label_text) + " ".html_safe + super
+    def with_label method, options
+      wrap_in_dfn = options.delete(:wrap_in_dfn)
+      label_class = options.delete(:label_class)
+      label_with_colon = options.delete(:label_with_colon)
+      label_text = options.delete(:label) || label_class.present? || label_with_colon
+
+      label_text = method.to_s.humanize if label_text === true
+      label_text&.sub!(/_id$/,"")
+      label_text << ":" if label_with_colon && !label_text.ends_with?(":")
+
+      field_html = yield
+
+      if label_text
+        if wrap_in_dfn
+          @template.tag.dfn do
+            @template.tag.dt(label(method, label_text, class: label_class)) +
+              @template.tag.dd(field_html)
+          end
+        else
+          label(method, label_text, class: label_class) + " ".html_safe + field_html
+        end
       else
+        field_html
+      end
+    end
+
+    def text_area method, options = {}
+      with_label(method, options) do
         super
       end
     end
 
     def date_field method, options = {}
-      options = options.dup
-      if label_text = options.delete(:label)
-        label_text = method.to_s.humanize if label_text === true
-        label(method, label_text) + " ".html_safe + super
-      else
+      with_label(method, options) do
+        super
+      end
+    end
+
+    def phone_field method, options = {}
+      with_label(method, options) do
         super
       end
     end
 
     def email_field method, options = {}
-      options = options.dup
-      if label_text = options.delete(:label)
-        label_text = method.to_s.humanize if label_text === true
-        label(method, label_text) + " ".html_safe + super
-      else
+      with_label(method, options) do
         super
       end
     end
 
     def text_field method, options = {}
-      options = options.dup
-      if label_text = options.delete(:label)
-        label_text = method.to_s.humanize if label_text === true
-        label(method, label_text) + " ".html_safe + super
-      else
+      with_label(method, options) do
         super
       end
     end
 
     def file_field method, options = {}
-      options = options.dup
-      if label_text = options.delete(:label)
-        label_text = method.to_s.humanize if label_text === true
-        label(method, label_text) + " ".html_safe + super
-      else
+      with_label(method, options) do
+        super
+      end
+    end
+
+    def select(method, choices = nil, options = {}, html_options = {}, &block)
+      label_options = (options.keys & %i[wrap_in_dfn label_class label_with_colon label]).any? ? options : html_options
+      with_label(method, label_options) do
+        super
+      end
+    end
+
+    def collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
+      label_options = (options.keys & %i[wrap_in_dfn label_class label_with_colon label]).any? ? options : html_options
+      with_label(method, label_options) do
+        super
+      end
+    end
+
+    def number_field method, options = {}
+      with_label(method, options) do
         super
       end
     end
@@ -62,11 +94,12 @@ module LabelledForm
     def radio_button(method, tag_value, options = {})
       options = options.dup
       label_text = options.delete(:label)
+      label_class = options.delete(:label_class)
       super.tap do |out|
         if label_text
           label_text = tag_value if label_text === true
           out << " ".html_safe
-          out << label(method, label_text, value: tag_value)
+          out << label(method, label_text, value: tag_value, class: label_class)
         end
       end
     end
@@ -74,14 +107,39 @@ module LabelledForm
     def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
       options = options.dup
       label_text = options.delete(:label)
+      label_class = options.delete(:label_class)
       super.tap do |out|
         if label_text
           label_text = checked_value == "1" ? nil : checked_value if label_text === true
           out << " ".html_safe
           label_options = options[:multiple] ? { value: checked_value } : {}
-          out << label(method, label_text, label_options)
+          out << label(method, label_text, label_options.merge(class: label_class))
         end
       end
+    end
+
+    def multiple_check_box method, value, options={}
+      options = options.dup
+      label_text = options.delete(:label)
+      label_class = options.delete(:label_class)
+      out = "".html_safe
+
+      if !multiple_check_box_fields.include?(method)
+        multiple_check_box_fields << method
+        out << hidden_field(method, value: "", multiple: true)
+      end
+
+      out << check_box(method, { multiple: true }.reverse_merge(options), value, nil)
+
+      if label_text
+        label_text = value == "1" ? nil : value if label_text === true
+        out << " ".html_safe
+        out << label(method, label_text, value: value, class: label_class)
+      end
+    end
+
+    private def multiple_check_box_fields
+      @multiple_check_box_fields ||= Set.new
     end
   end
 end
